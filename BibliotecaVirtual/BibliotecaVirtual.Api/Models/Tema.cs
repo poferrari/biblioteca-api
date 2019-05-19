6972d9cba849d6d2f6073097ddbb6204e3411a3d
JOAO VITOR FERRARI DA SILVA <poferrari@gmail.com>
2019-05-19 16:25:42 -0300

    Acréscimo das Models.

---
+++ BibliotecaVirtual/BibliotecaVirtual.Api/Models/Tema.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BibliotecaVirtual.Api.Models
+{
+    public class Tema
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
